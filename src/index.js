a69da256b03c300d8f5aa5e1c8baf0b8ee39db4e
Ngenda <ngenda@localhost.localdomain>
2019-11-27 17:03:15 +0300

    Initial project setup :sparkles:

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return (<div>
+    <h3>Bawabu client</h3>
+  </div>);
+};
+
+ReactDom.render(<App/>, document.getElementById('root'));
